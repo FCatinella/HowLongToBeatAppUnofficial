5b017a0677bcec5830d63835c0ebfe08d8e9f096
Fabio <fabio@beeapp.org>
2022-02-27 18:54:38 +0100

    Updated Game backgrounds and extracted them

--- app/src/main/java/com/fabiocati/howlongtobeatunofficial/Constants.kt
+++ app/src/main/java/com/fabiocati/howlongtobeatunofficial/Constants.kt
@@ -4,4 +4,20 @@ object Constants {
     const val GAME_NAME = "GAME_NAME"
     const val GAME_ID = "GAME_ID"
     const val BASE_URL = "https://howlongtobeat.com"
+
+    val GAME_BACK_LINKS = arrayOf(
+        "https://www.cyberludus.com/content/uploads/2018/04/godofwar_wall.jpg", //God of war
+        "https://www.optimagazine.com/wp-content/uploads/2019/02/resident-evil-2-remake-recensione.jpg", // RE 2 remake
+        "https://gametimes.com.br/wp-content/uploads/2020/04/wh3h15zelyg41-scaled.jpg", //Tlou 2
+        "https://images.alphacoders.com/789/thumb-1920-789452.jpg", //Breath of the wild
+        "https://www.cyberludus.com/content/uploads/2018/11/750703.jpg", //Rdd2
+        "https://images.pushsquare.com/96299803f0808/demons-souls-ps5.original.jpg", // Demon's souls
+        "https://images5.alphacoders.com/927/thumb-1920-927025.png", // Cyberpunk 2077
+        "https://cs10.pikabu.ru/post_img/big/2020/04/11/2/1586567698133226382.jpg",// FFVII remake
+        "https://image.api.playstation.com/vulcan/ap/rnd/202010/2915/SqRcyLjZbpK26ej6TnWf43xp.jpg", // Horizon 2
+        "https://acquisti.corriere.it/wp-content/uploads/2021/09/death-stranding-directors-cut.jpg", // Death Stranding
+        "https://www.vgn.it/wp-content/uploads/2020/10/crash_4_its_about_time_cover.jpg", // Crash 4
+        "https://sm.ign.com/ign_it/blogroll/b/bit-takes-/bit-takes-twobbrbrit-takes-two-tells-the-story-of-a-married_pdew.jpg", // It Takes Two
+        "https://static.bandainamcoent.eu/high/elden-ring/elden-ring/00-page-setup/elden-ring-new-header-mobile.jpg" // Elden Ring
+    )
 }
--- app/src/main/java/com/fabiocati/howlongtobeatunofficial/GameRepository.kt
+++ app/src/main/java/com/fabiocati/howlongtobeatunofficial/GameRepository.kt
@@ -14,19 +14,6 @@ class GameRepository @Inject constructor(
 
     private val cacheMap = HashMap<Int, GameDetailsModel>()
 
-    companion object {
-        private val gameBackLink = arrayOf(
-            "https://www.cyberludus.com/content/uploads/2018/04/godofwar_wall.jpg", //God of war
-            "https://www.optimagazine.com/wp-content/uploads/2019/02/resident-evil-2-remake-recensione.jpg", // RE 2 remake
-            "https://gametimes.com.br/wp-content/uploads/2020/04/wh3h15zelyg41-scaled.jpg", //Tlou 2
-            "https://images.alphacoders.com/789/thumb-1920-789452.jpg", //Breath of the wild
-            "https://www.cyberludus.com/content/uploads/2018/11/750703.jpg", //Rdd2
-            "https://images.pushsquare.com/96299803f0808/demons-souls-ps5.original.jpg", // Demon's souls
-            "https://images.hdqwalls.com/download/cyberpunk-2077-johnny-silverhand-4k-lt-3840x2160.jpg", // Cyberpunk 2077
-            "https://cs10.pikabu.ru/post_img/big/2020/04/11/2/1586567698133226382.jpg" // FFVII remake
-        )
-    }
-
     fun getGame(gameId: Int): GameDetailsModel {
         var game = cacheMap[gameId]
         if (game == null) {
@@ -40,7 +27,7 @@ class GameRepository @Inject constructor(
     }
 
     fun getBackground(): String {
-        val random = Random.nextInt(0, gameBackLink.size)
-        return gameBackLink[random]
+        val random = Random.nextInt(0, Constants.GAME_BACK_LINKS.size)
+        return Constants.GAME_BACK_LINKS[random]
     }
 }
